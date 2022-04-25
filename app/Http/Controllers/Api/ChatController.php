98e37ed6480c6ac2832201c81daa7c10d2caf985
Milk0FF <ivan.milko@yandex.ru>
2022-04-26 00:18:19 +0600

    Inititialize start task methods

---
+++ app/Http/Controllers/Api/ChatController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+
+    public function createChat()
+    {
+
+    }
+    public function createMessage()
+    {
+
+    }
+
+    public function updateMessage()
+    {
+
+    }
+
+    public function deleteMessage()
+    {
+
+    }
+
+    public function getChats()
+    {
+
+    }
+
+    public function getChatMessages()
+    {
+
+    }
+    public function getChatsByTask()
+    {
+
+    }
+}
